82874eadd53f74b966593f8a1db37ab6ceb3cc6a
Sbadweh <sheenaymudwe@gmail.com>
2025-10-23 22:21:00 -0400

    add Debug to gitignore

--- Badweh_development/Debug/modules/stat/stat.d
+++
@@ -1,5 +0,0 @@
-modules/stat/stat.o: ../modules/stat/stat.c \
- C:/Users/Sheen/Desktop/Embedded_System/gene_Baremetal_I2CTmphm_RAM_CICD/Badweh_Development/modules/include/tmr.h \
- C:/Users/Sheen/Desktop/Embedded_System/gene_Baremetal_I2CTmphm_RAM_CICD/Badweh_Development/modules/include/stat.h
-C:/Users/Sheen/Desktop/Embedded_System/gene_Baremetal_I2CTmphm_RAM_CICD/Badweh_Development/modules/include/tmr.h:
-C:/Users/Sheen/Desktop/Embedded_System/gene_Baremetal_I2CTmphm_RAM_CICD/Badweh_Development/modules/include/stat.h:
