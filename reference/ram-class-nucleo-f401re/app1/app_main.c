4e15d87f377de6a7da5b737186d39743f6478fb6
Sbadweh <sheenaymudwe@gmail.com>
2025-11-21 14:09:36 -0500

    Modify app_main.c from mod_info into explicitly init, start, and run

--- reference/ram-class-nucleo-f401re/app1/app_main.c
+++ reference/ram-class-nucleo-f401re/app1/app_main.c
@@ -52,43 +52,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-// Type definitions
-////////////////////////////////////////////////////////////////////////////////
-
-#define MOD_NO_INSTANCE -1
-
-typedef int32_t (*mod_get_def_cfg)(void* cfg);
-typedef int32_t (*mod_init)(void* cfg);
-typedef int32_t (*mod_start)(void);
-typedef int32_t (*mod_run)(void);
-
-typedef int32_t (*mod_instance_get_def_cfg)(int instanced, void* cfg);
-typedef int32_t (*mod_instance_init)(int instance, void* cfg);
-typedef int32_t (*mod_instance_start)(int instance);
-typedef int32_t (*mod_instance_run)(int instance);
-
-struct mod_info {
-    const char* name;
-    int instance;
-    union {
-        struct {
-            mod_get_def_cfg mod_get_def_cfg;
-            mod_init mod_init;
-            mod_start mod_start;
-            mod_run mod_run;
-        } singleton;
-        struct {
-            mod_instance_get_def_cfg mod_get_def_cfg;
-            mod_instance_init mod_init;
-            mod_instance_start mod_start;
-            mod_instance_run mod_run;
-        } multi_instance;
-    } ops;
-    void* cfg_obj;
-};
-
-
-////////////////////////////////////////////////////////////////////////////////
 // Private (static) function declarations
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -97,7 +60,6 @@ static int32_t cmd_main_status();
 ////////////////////////////////////////////////////////////////////////////////
 // Private (static) variables
 ////////////////////////////////////////////////////////////////////////////////
-
 
 static struct cmd_cmd_info cmds[] = {
     {
@@ -118,91 +80,6 @@ static struct i2c_cfg i2c_cfg;
 static struct ttys_cfg ttys_cfg_2;
 static struct tmphm_cfg tmphm_cfg;
 
-
-static struct mod_info mods[] = {
-
-    {
-        .name = "ttys",
-        .instance = TTYS_INSTANCE_2,
-        .ops.multi_instance.mod_get_def_cfg =
-            (mod_instance_get_def_cfg)ttys_get_def_cfg,
-        .ops.multi_instance.mod_init = (mod_instance_init)ttys_init,
-        .ops.multi_instance.mod_start = (mod_instance_start)ttys_start,
-        .cfg_obj = &ttys_cfg_2,
-    },
-
-    {
-        .name = "fault",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_init = (mod_init)fault_init,
-        .ops.singleton.mod_start = (mod_start)fault_start,
-    },
-    {
-        .name = "flash",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_start = (mod_start)flash_start,
-    },
-
-    {
-        .name = "lwl",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_start = (mod_start)lwl_start,
-    },
-
-
-    {
-        .name = "wdg",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_init = (mod_init)wdg_init,
-        .ops.singleton.mod_start = (mod_start)wdg_start,
-    },
-    {
-        .name = "cmd",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_init = (mod_init)cmd_init,
-    },
-    {
-        .name = "console",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_get_def_cfg = (mod_get_def_cfg)console_get_def_cfg,
-        .ops.singleton.mod_init = (mod_init)console_init,
-        .ops.singleton.mod_run = (mod_run)console_run,
-        .cfg_obj = &console_cfg,
-    },
-    {
-        .name = "tmr",
-        .instance = MOD_NO_INSTANCE,
-        .ops.singleton.mod_init = (mod_init)tmr_init,
-        .ops.singleton.mod_start = (mod_start)tmr_start,
-        .ops.singleton.mod_run = (mod_run)tmr_run,
-    },
-
-    {
-        .name = "i2c",
-        .instance = I2C_INSTANCE_3,
-        .ops.singleton.mod_get_def_cfg = (mod_get_def_cfg)i2c_get_def_cfg,
-        .ops.singleton.mod_init = (mod_init)i2c_init,
-        .ops.singleton.mod_start = (mod_start)i2c_start,
-        .cfg_obj = &i2c_cfg,
-    },
-
-    {
-        .name = "tmphm",
-        .instance = TMPHM_INSTANCE_1,
-        .ops.singleton.mod_get_def_cfg = (mod_get_def_cfg)tmphm_get_def_cfg,
-        .ops.singleton.mod_init = (mod_init)tmphm_init,
-        .ops.singleton.mod_start = (mod_start)tmphm_start,
-        .ops.singleton.mod_run = (mod_run)tmphm_run,
-        .cfg_obj = &tmphm_cfg,
-    },
-
-
-
-
-
-
-};
-
 ////////////////////////////////////////////////////////////////////////////////
 // Public (global) variables and externs
 ////////////////////////////////////////////////////////////////////////////////
@@ -213,115 +90,71 @@ static struct mod_info mods[] = {
 
 void app_main(void)
 {
-    // int32_t result;;
-    int32_t rc;
-    int32_t idx;
-    struct mod_info* mod;
-
-
+    struct tmr_cfg tmr_cfg;
+    struct fault_cfg fault_cfg;
+    struct wdg_cfg wdg_cfg;
 
     wdg_start_init_hdw_wdg();
 
-    lwl_enable(true);
-
-
-    //
-    // Invoke the init API on modules the use it.
-    //
+    memset(&tmr_cfg, 0, sizeof(tmr_cfg));
+    memset(&fault_cfg, 0, sizeof(fault_cfg));
+    memset(&wdg_cfg, 0, sizeof(wdg_cfg));
 
     setvbuf(stdout, NULL, _IONBF, 0);
     printc("\nInit: Init modules\n");
 
-    for (idx = 0, mod = mods;
-         idx < ARRAY_SIZE(mods);
-         idx++, mod++) {
-        if (mod->ops.singleton.mod_get_def_cfg != NULL &&
-            mod->cfg_obj != NULL) {
-            if (mod->instance == MOD_NO_INSTANCE) {
-                rc = mod->ops.singleton.mod_get_def_cfg(mod->cfg_obj);
-            } else {
-                rc = mod->ops.multi_instance.mod_get_def_cfg(mod->instance,
-                                                             mod->cfg_obj);
-            }
+    tmr_init(&tmr_cfg);
 
-        }
-    }
+    ttys_get_def_cfg(TTYS_INSTANCE_2, &ttys_cfg_2);
+    ttys_init(TTYS_INSTANCE_2, &ttys_cfg_2);
 
+    fault_init(&fault_cfg);
 
+    cmd_init(NULL);
+
+    console_get_def_cfg(&console_cfg);
+    console_init(&console_cfg);
+
+    i2c_get_def_cfg(I2C_INSTANCE_3, &i2c_cfg);
+    i2c_init(I2C_INSTANCE_3, &i2c_cfg);
+
+    tmphm_get_def_cfg(TMPHM_INSTANCE_1, &tmphm_cfg);
     tmphm_cfg.i2c_instance_id = I2C_INSTANCE_3;
-
-
-    for (idx = 0, mod = mods;
-         idx < ARRAY_SIZE(mods);
-         idx++, mod++) {
-        if (mod->ops.singleton.mod_init != NULL) {
-            if (mod->instance == MOD_NO_INSTANCE) {
-                rc = mod->ops.singleton.mod_init(mod->cfg_obj);
-            } else {
-                rc = mod->ops.multi_instance.mod_init(mod->instance,
-                                                      mod->cfg_obj);
-            }
-
-        }
-    }
-
-    //
-    // Invoke the start API on modules the use it.
-    //
+    tmphm_init(TMPHM_INSTANCE_1, &tmphm_cfg);
 
     printc("Init: Start modules\n");
 
-    for (idx = 0, mod = mods;
-         idx < ARRAY_SIZE(mods);
-         idx++, mod++) {
-        if (mod->ops.singleton.mod_start != NULL) {
-            if (mod->instance == MOD_NO_INSTANCE) {
-                rc = mod->ops.singleton.mod_start();
-            } else {
-                rc = mod->ops.multi_instance.mod_start(mod->instance);
-            }
+    ttys_start(TTYS_INSTANCE_2);
+    fault_start();
+    flash_start();
+    lwl_start();
+    lwl_enable(true);
+    wdg_init(&wdg_cfg);
+    wdg_start();
+    tmr_start();
+    i2c_start(I2C_INSTANCE_3);
+    tmphm_start(TMPHM_INSTANCE_1);
 
-        }
-                
-    }
-
-    rc = cmd_register(&cmd_info);
-
-
-
-
-    //
-    // In the super loop invoke the run API on modules the use it.
-    //
-
-    wdg_init_successful();
-    rc = wdg_start_hdw_wdg(CONFIG_WDG_HARD_TIMEOUT_MS);
+    cmd_register(&cmd_info);
 
     printc("Init: Enter super loop\n");
+    wdg_init_successful();
+    wdg_start_hdw_wdg(CONFIG_WDG_HARD_TIMEOUT_MS);
+
+    printc("\n[READY] Entering super loop...\n");
+    printc("TMPHM running in background.\n");
+    printc("Console commands available:\n");
+    printc("  - tmphm status\n");
+    printc("  - tmphm test lastmeas 0\n");
+    printc("  - i2c status\n\n");
+
     while (1)
     {
-
-
-        for (idx = 0, mod = mods;
-             idx < ARRAY_SIZE(mods);
-             idx++, mod++) {
-            if (mod->ops.singleton.mod_run != NULL) {
-                if (mod->instance == MOD_NO_INSTANCE) {
-                    rc = mod->ops.singleton.mod_run();
-                } else {
-                    rc = mod->ops.multi_instance.mod_run(mod->instance);
-                }
-                if (rc < 0) {
-                	printc("Run Error\n");
-                }
-
-            }
-        }
+        console_run();
+        tmr_run();
+        tmphm_run(TMPHM_INSTANCE_1);
     }
 }
-
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Private (static) functions
 ////////////////////////////////////////////////////////////////////////////////
